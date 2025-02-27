646900f4903067de5a72e783cdc97837ef067f7d
Michal Lukaszewicz <michal.lukaszewicz@precast.pl>
2025-02-27 20:29:48 +0100

    Uwagi

---
+++ BlackJackbis/Card.cs
@@ -0,0 +1,15 @@
+﻿namespace BlackJackbis
+{
+    public class Card
+    {
+        public Kolory Color { get; }
+
+        public Figury Figure { get; }
+
+        public Card(Figury figure, Kolory color)
+        {
+            Figure = figure;
+            Color = color;
+        }
+    }
+}
---
+++ BlackJackbis/CountPointsService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackbis
+{
+    //W programowaniu jest coś takiego jak wzorce projektowe - nie musisz na razie ich znać, po prostu dobrze żebyś wiedział że istnieją - są to takie "schematy" które
+    // bardzo często się powtarzają i mają już swoje nazwy. Np. decorator - bierze istniejącą już klasę, pobiera ją w konstruktor i na jej podstawie
+    //w swoich własnych metodach rozszerza jej funkcje. To jest klasa która coś tam liczy i wyświetla wartość. Takie klasy które robią coś konkretnego (np. coś liczą)
+    //często nazywamy po prostu Service
+    public class CountPointsService
+    {
+        //te metody i lista mogłyby też nie być statyczne. Ale w tym wypadku wydaje mi się że mogłyby zostać
+        //Po co wgl ta lista? nie jest nigdzie używana
+        private static List<Figury> ListaPunktow = new List<Figury>();
+
+        //Wyrzuciłem jednak static bo chciałem to zapakować w stół
+        //Zmieniłem też parametr nad Card - będize tak mi łatwiej, bez sensu wyciągać te listę figur w obecnej sytuacji
+        public int WyswietlanieWartosciKart(List<Card> figuryNaStole)
+        {
+            //ListaPunktow.Add(figury);
+            int punkty = figuryNaStole.Sum(f => (int)f.Figure);
+            Console.WriteLine($"Punkty  {punkty}");
+            return punkty;
+        }
+    }
+}
